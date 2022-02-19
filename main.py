33e5e6a290b70255f2cbf40309cb87eaf66456d1
olonok69 <olonok@gmail.com>
2022-02-19 15:56:11 +0000

    Include main and utils

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from unityagents import UnityEnvironment
 from mad4pg_agent import MAD4PG   , train_mad4pg
-import numpy as np
+from utils import *
 
 env = UnityEnvironment(file_name="./Tennis_Windows_x86_64/Tennis.exe")
 
@@ -15,3 +15,6 @@ epi_scores = train_mad4pg(agent, n_agents=2, n_episodes=10000, env=env, brain_na
 
 epi_scores = np.array(epi_scores)
 np.save('./outputs/epi_scores.npy', epi_scores)
+
+epi_scores = np.load('./outputs/epi_scores.npy')
+plot_scores(epi_scores)
